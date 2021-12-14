faa1016ba410b735d8038119a47c7c55006000e2
Evgzek <zekamusijhcuk@gmail.com>
2021-12-14 11:50:07 +1000

    Сортировка массива

--- src/Bubble.java
+++ src/Bubble.java
@@ -12,5 +12,15 @@ public class Bubble {
             array[i] = p.nextInt();
         }
         System.out.println( "Ваш массив: "+ Arrays.toString(array));
+        for (int i = a-1; i>=1; i--){
+            for (int g = 0; g<i; g++){
+                if(array[g]>array[g+1]){
+                    int e = array[g];
+                    array[g] = array[g+1];
+                    array[g+1] = e;
+                }
+            }
+        }
+        System.out.println("Массив прошедший сортировку: " + Arrays.toString(array));
     }
 }
